f3adf52823265945889f8de0de934d745706909c
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2022-07-31 13:32:51 +0430

    Big Bang

---
+++ webpack/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+module.exports = {
+    mode: "production",
+    entry: {
+        injectScript: path.resolve(__dirname, "..", "src", "injectScript.ts"),
+    },
+    output: {
+        path: path.join(__dirname, "../dist"),
+        filename: "[name].js",
+    },
+    resolve: {
+        extensions: [".ts", ".js"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [{from: ".", to: ".", context: "public"}]
+        }),
+    ],
+    optimization: {
+        minimize: true
+    },
+};
