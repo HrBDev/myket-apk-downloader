530773e6e321f24e9047da53dbe1b0b937202fcb
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2022-08-21 21:05:05 +0430

    Configure prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,14 @@
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    extends: ['eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'prettier',
+    ],
     parser: '@typescript-eslint/parser',
+    parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.json'],
+    },
     plugins: ['@typescript-eslint'],
     root: true,
 };
