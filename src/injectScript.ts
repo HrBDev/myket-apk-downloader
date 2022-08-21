64a95f4eb944f5c6dc8c1a084f275aa8b63a47db
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2022-08-22 00:16:53 +0430

    Tweak prettierrc

--- src/injectScript.ts
+++ src/injectScript.ts
@@ -1,28 +1,28 @@
 function waitForElement(selector: string): Promise<Element> {
-    return new Promise<Element>((resolve) => {
+    return new Promise<Element>(resolve => {
         if (document.querySelector(selector)) {
-            return resolve(document.querySelector(selector)!);
+            return resolve(document.querySelector(selector)!)
         }
 
-        const observer = new MutationObserver((_) => {
+        const observer = new MutationObserver(_ => {
             if (document.querySelector(selector)) {
-                resolve(document.querySelector(selector)!);
-                observer.disconnect();
+                resolve(document.querySelector(selector)!)
+                observer.disconnect()
             }
-        });
+        })
 
         observer.observe(document.body, {
             childList: true,
             subtree: true
-        });
-    });
+        })
+    })
 }
 
 const Auth = ""
     
     
     
-    ;
+    
 
 const Header = {
     Accept: "application/json",
@@ -32,65 +32,63 @@ const Header = {
     Host: "apiserver.myket.ir",
     Connection: "Keep-Alive",
     "Accept-Encoding": "gzip"
-};
+}
 
-const v1BaseUrl = "https://apiserver.myket.ir/v1/applications";
-const v2BaseUrl = "https://apiserver.myket.ir/v2/applications";
+const v1BaseUrl = "https://apiserver.myket.ir/v1/applications"
+const v2BaseUrl = "https://apiserver.myket.ir/v2/applications"
 
 waitForElement("a.btn-download")
     .then(getDownloadLink)
-    .catch((_) => {
-        console.log("Could not find download button.");
-    });
+    .catch(_ => console.log("Could not find download button."))
 
 async function getDownloadLink(downloadBtn: Element) {
-    const btnSpan = document.getElementById("basebtn");
+    const btnSpan = document.getElementById("basebtn")
     if (!btnSpan) {
-        console.log("Could not find download button text element.");
-        return;
+        console.log("Could not find download button text element.")
+        return
     }
     try {
-        const url: URL = new URL(downloadBtn.getAttribute("href")!);
-        const pkgName: string = url.searchParams.get("packageName")!;
-        const infoUrl = `${v2BaseUrl}/${pkgName}/`;
+        const url: URL = new URL(downloadBtn.getAttribute("href")!)
+        const pkgName: string = url.searchParams.get("packageName")!
+        const infoUrl = `${v2BaseUrl}/${pkgName}/`
         const infoRes = await fetch(infoUrl, {
             mode: "cors",
             method: "GET",
             headers: Header
-        });
-        if (!infoRes.ok) throw new Error("Request failure.");
+        })
+        if (!infoRes.ok) throw new Error("Request failure.")
         // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
         const {
             price: { isFree },
             size: { actual },
             version: { code }
         }: {
-            price: { isFree: boolean };
-            size: { actual: number };
-            version: { code: string };
-        } = await infoRes.json();
+            price: { isFree: boolean }
+            size: { actual: number }
+            version: { code: string }
+        } = await infoRes.json()
         if (!isFree) {
-            console.log("Paid App!");
-            return;
+            console.log("Paid App!")
+            return
         }
         const v1Url =
             `${v1BaseUrl}/${pkgName}/uri/?action=start&` +
-            `requestedVersion=${code}&fileType=App&lang=fa`;
+            `requestedVersion=${code}&fileType=App&lang=fa`
         const v1Res = await fetch(v1Url, {
             mode: "cors",
             method: "GET",
             headers: Header
-        });
+        })
         // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        const { uri }: { uri: string } = await v1Res.json();
-        console.log(`APK Download link: ${uri}`);
-        if (!uri) throw new Error("No download link.");
-        downloadBtn.removeAttribute("onclick");
-        downloadBtn.setAttribute("href", uri);
-        btnSpan.textContent = `دانلود (${actual})`;
+        const { uri }: { uri: string } = await v1Res.json()
+        console.log(`APK Download link: ${uri}`)
+        if (!uri) throw new Error("No download link.")
+        downloadBtn.removeAttribute("onclick")
+        downloadBtn.setAttribute("href", uri)
+        btnSpan.textContent = `دانلود (${actual})`
     } catch (e) {
-        btnSpan.textContent = "خطا";
-        downloadBtn.setAttribute("href", "#");
-        console.log(e);
+        btnSpan.textContent = "خطا"
+        downloadBtn.setAttribute("href", "#")
+        console.log(e)
     }
 }
