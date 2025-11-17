1a9f38c2af666f7609979ae5b0eedea3548f5925
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2025-11-17 23:44:09 +0330

    Refactor test.js to improve structure and readability of integration tests

--- tests/test.js
+++ tests/test.js
@@ -3,44 +3,39 @@ import * as path from "path"
 import * as url from "url"
 import { expect, assert } from "chai"
 
-let browserPage = null
-let browser = null
+let browser, browserPage
 
 describe("Extension Integration Testing", function () {
-	this.timeout(25000) // wait for browser to open
-	before(async () => await setup())
+	this.timeout(25000)
 
-	describe("App Page", async function () {
-		it("Check for a valid url", async function () {
-			const inputElement =
-				await browserPage.waitForSelector("#mainInstall")
-			assert.ok(inputElement)
-			await new Promise(r => setTimeout(r, 5000))
-			expect(await inputElement.evaluate(el => el.href)).match(
-				RegExp("^https:\\/\\/.{3}-data-\\d+\\.myket\\.ir"),
-			)
+	before(async () => {
+		const __dirname = url.fileURLToPath(new URL(".", import.meta.url))
+		const extensionPath = path.join(__dirname, "..", "dist")
+		browser = await puppeteer.launch({
+			executablePath: process.env.PUPPETEER_EXEC_PATH,
+			headless: false,
+			enableExtensions: [extensionPath],
+			pipe: true,
+			args: [
+				"--no-sandbox",
+				"--disable-setuid-sandbox",
+				"--font-render-hinting=none",
+			],
 		})
+		browserPage = await browser.newPage()
+		await browserPage.goto(
+			"https://myket.ir/app/com.sibche.aspardproject.app",
+		)
+	})
+
+	it("Check for a valid url", async function () {
+		const inputElement = await browserPage.waitForSelector("#mainInstall")
+		assert.ok(inputElement)
+		await new Promise(r => setTimeout(r, 5000))
+		expect(await inputElement.evaluate(el => el.href)).match(
+			/^https:\/\/.{3}-data-\d+\.myket\.ir/,
+		)
 	})
 
 	after(async () => await browser.close())
 })
-
-async function setup() {
-	const __dirname = url.fileURLToPath(new URL(".", import.meta.url))
-	let extensionPath = path.join(__dirname, "..", "dist")
-	console.log(extensionPath)
-	browser = await puppeteer.launch({
-		executablePath: process.env.PUPPETEER_EXEC_PATH,
-		headless: false,
-		enableExtensions: [extensionPath],
-		pipe: true,
-		args: [
-			"--no-sandbox",
-			"--disable-setuid-sandbox",
-			"--font-render-hinting=none",
-		],
-	})
-
-	browserPage = await browser.newPage()
-	await browserPage.goto("https://myket.ir/app/com.sibche.aspardproject.app")
-}
